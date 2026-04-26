165aa5da4ff3a9859decc76d58d608bb1c2cb5f1
vladimir <artem-r2012@mail.ru>
2026-04-26 11:51:24 +0300

    test 3

--- tests/factories.py
+++ tests/factories.py
@@ -1,31 +1,31 @@
-import factory
-#import factory.fuzzy as fuzzy
-import random
-#from datetime import datetime
-
-from module_2.hw_29.parking.main.models import Clients, Parking
-from module_2.hw_29.parking.main.app import db
-
-
-
-class ClientFactory(factory.alchemy.SQLAlchemyModelFactory):
-    class Meta:
-        model = Clients
-        sqlalchemy_session = db.session
-
-    name = factory.Faker('first_name')
-    surname = factory.Faker('last_name')
-    credit_card = factory.Faker("boolean")
-    car_number = factory.Faker('license_plate', locale='ru_RU')
-
-
-
-class ParkingFactory(factory.alchemy.SQLAlchemyModelFactory):
-    class Meta:
-        model = Parking
-        sqlalchemy_session = db.session
-
-    address = factory.Faker("address")
-    opened = factory.Faker("boolean")
-    count_places = factory.Faker('random_int', min=20, max=50)
-    count_available_places = factory.LazyAttribute(lambda x: random.randrange(1, 20))
+# import factory
+# #import factory.fuzzy as fuzzy
+# import random
+# #from datetime import datetime
+#
+# from module_2.hw_29.parking.main.models import Clients, Parking
+# from module_2.hw_29.parking.main.app import db
+#
+#
+#
+# class ClientFactory(factory.alchemy.SQLAlchemyModelFactory):
+#     class Meta:
+#         model = Clients
+#         sqlalchemy_session = db.session
+#
+#     name = factory.Faker('first_name')
+#     surname = factory.Faker('last_name')
+#     credit_card = factory.Faker("boolean")
+#     car_number = factory.Faker('license_plate', locale='ru_RU')
+#
+#
+#
+# class ParkingFactory(factory.alchemy.SQLAlchemyModelFactory):
+#     class Meta:
+#         model = Parking
+#         sqlalchemy_session = db.session
+#
+#     address = factory.Faker("address")
+#     opened = factory.Faker("boolean")
+#     count_places = factory.Faker('random_int', min=20, max=50)
+#     count_available_places = factory.LazyAttribute(lambda x: random.randrange(1, 20))
